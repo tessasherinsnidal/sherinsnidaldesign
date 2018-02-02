f826833a2ba7b28fa83a076339a8eda30b3d347b
tessasherinsnidal <tessasherinsnidal@icloud.com>
2018-02-02 10:41:24 -0500

    create

---
+++ README.md
@@ -0,0 +1,15 @@
+# sherinsnidaldesign.ca
+
+#### [sherinsnidaldesign.ca](https://sherinsnidaldesign.ca)
+
+![]
+
+Tessa Sherin-Snidal
+
+The opportunity to solve problems in a constructive, and creative way is what gets me out of bed in the morning. Graphic Design provides me these opportunities and I love every day I am able to practice my profession. It drives me to better myself, and the world around me.
+
+I have always loved art, but have never been able to just make something attractive without a purpose. Through my art I am able to solve usability issues facing our everyday life.
+
+- **[tessa@sherinsnidaldesign.ca](mailto:tessa@sherinsnidaldesign.ca)
+- [Instagram](https://www.instagram.com/sherinsnidaldesign/)
+- [Behance](https://www.behance.net/tsherinsnidal)
---
+++ js/main.js
@@ -0,0 +1,138 @@
+/*for gallery click*/
+
+function openImg2(imgName) {
+  var i, x;
+  x = document.getElementsByClassName("picture2");
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  document.getElementById(imgName).style.display = "block";
+}
+function openImg(imgName) {
+  var i, x;
+  x = document.getElementsByClassName("picture");
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  document.getElementById(imgName).style.display = "block";
+}
+
+/*for dropdown click*/
+
+function myFunction() {
+    var x = document.getElementById("Demo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function myFunction2() {
+    var x = document.getElementById("Demo2");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function myFunction3() {
+    var x = document.getElementById("Demo3");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function myFunction4() {
+    var x = document.getElementById("Demo4");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function myFunction5() {
+    var x = document.getElementById("Demo5");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function myFunction6() {
+    var x = document.getElementById("Demo6");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+/*for page loader read load*/
+
+window.onload = function() {
+  document.getElementById('body').className = 'loaded';
+};
+
+/*for click next slideshow*/
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " w3-white";
+}
+
+var slideIndex = 1;
+showDivs2(slideIndex);
+
+function plusDivs2(n) {
+  showDivs2(slideIndex += n);
+}
+
+function currentDiv2(n) {
+  showDivs2(slideIndex = n);
+}
+
+function showDivs2(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides2");
+  var dots = document.getElementsByClassName("demo2");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" w3-black", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " w3-black";
+}
+
+/*For Video*/
